2bf9133f5f58192527b9cf1679b5afa7a8c305ee
Faizaan Pervaiz <mfaizk2008@gmail.com>
2021-01-24 02:09:05 +0000

    ci(frontend): env var Firebase config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,15 @@ import store from './store'
 
 import firebase from "firebase";
 
-// TODO: env var Firebase config
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
 
 firebase.initializeApp(firebaseConfig);
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
