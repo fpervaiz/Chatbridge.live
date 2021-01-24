6a98ba8a74184023aa17377aa0de32e4a708c8a0
Faizaan Pervaiz <mfaizk2008@gmail.com>
2021-01-24 21:59:09 +0000

    fix(frontend): production site coming soon

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -7,8 +7,10 @@
       <v-row justify="center" no-gutters>
         <v-btn color="white" text class="my-1"> About </v-btn>
         <v-btn color="white" text class="my-1"> Contact </v-btn>
+        <!--
         <v-btn color="white" text class="my-1"> Terms Of Use </v-btn>
         <v-btn color="white" text class="my-1"> Privacy Policy </v-btn>
+        -->
       </v-row>
     </v-container>
     <!--
---
+++ frontend/src/components/HomePage_ComingSoon.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row class="mt-5 text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Chat anonymously with Cambridge students.
+        </h1>
+
+        <h2 class="subheading font-weight-regular">Simple, safe, secure.</h2>
+        <hr class="my-5" />
+        <p>
+          Only verified University of Cambridge students can use UniOfCam.Chat.
+        </p>
+        <v-btn class="mx-2" color="primary">Coming Soon!</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+</style>
