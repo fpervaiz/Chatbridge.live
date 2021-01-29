480ff00c330a60387ec988b14c1647c0007a4cec
Faizaan Pervaiz <fp361@cam.ac.uk>
2021-01-29 03:05:49 +0000

    feat(backend): use firebase service account

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,11 @@ const { v4: uuidv4 } = require('uuid')
 const generateName = require('sillyname')
 const Denque = require("denque")
 
-var firebaseConfig = process.env.FIREBASE_CONFIG ? JSON.parse(process.env.FIREBASE_CONFIG) : {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+var serviceAccount = process.env.FIREBASE_CONFIG_B64 ? JSON.parse(Buffer.from(process.env.FIREBASE_CONFIG_B64, 'base64').toString('ascii')) : require("./firebaseSA.json")
 
-var firebase = admin.initializeApp(firebaseConfig)
+var firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 var userQueue = new Denque()
 var userQueueCache = new Set()
