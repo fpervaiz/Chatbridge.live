688eb6dd6541cdf2b4656a9d3cc271cd663c0d75
Faizaan Pervaiz <fp361@cam.ac.uk>
2021-02-13 22:39:54 +0000

    fix(frontend): disable footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,20 +6,20 @@
         <router-view />
       </transition>
     </v-main>
-    <Footer />
+    <!-- <Footer /> -->
   </v-app>
 </template>
 
 <script>
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 export default {
   name: "App",
 
   components: {
     NavBar,
-    Footer,
+    // Footer,
   },
 
   data: () => ({
