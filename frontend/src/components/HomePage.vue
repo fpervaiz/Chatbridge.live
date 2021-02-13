f0c955078710ebe1efbcd6b1f466943d86ea8a22
Faizaan Pervaiz <fp361@cam.ac.uk>
2021-02-13 23:46:15 +0000

    feat(frontend): homepage subtitles

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -3,14 +3,12 @@
     <v-row class="mt-5 text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Chat anonymously with Cambridge students.
+          Chat anonymously with other Cambridge students.
         </h1>
 
-        <h2 class="subheading font-weight-regular">Simple, safe, secure.</h2>
+        <h2 class="subheading font-weight-regular">{{ subtitle }}</h2>
         <hr class="my-5" />
-        <p>
-          Only verified University of Cambridge students can use Chatbridge.
-        </p>
+        <p>You must be a University of Cambridge student to use Chatbridge.</p>
         <v-btn to="/chat" class="mx-2" color="primary">Start chatting</v-btn>
         <v-btn to="/about" class="mx-2" color="secondary">Tell me more</v-btn>
       </v-col>
@@ -21,6 +19,25 @@
 <script>
 export default {
   name: "HomePage",
+
+  data() {
+    return {
+      subtitles: [
+        "Rumour has it Stephen Toope is on here.",
+        "Find the one you've been looking for.",
+        "Scientifically proven to be more fun than binging lectures.",
+        "Isn't the name clever?",
+        "Ain't no pandemic large enough.",
+      ],
+      subtitle: null,
+    };
+  },
+
+  created() {
+    this.subtitle = this.subtitles[
+      Math.floor(Math.random() * this.subtitles.length)
+    ];
+  },
 };
 </script>
 
