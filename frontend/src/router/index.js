64201cc34809f01f0e80b896aafaa593daff6a0f
Faizaan Pervaiz <fp361@cam.ac.uk>
2021-02-16 18:37:21 +0000

    chore(frontend): disable general login for launch

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 const HomePage = () => import('./../components/HomePage.vue')
 const About = () => import('./../components/About.vue')
 const LoginRaven = () => import('./../components/LoginRaven.vue')
-const Login = () => import('./../components/Login.vue')
+// const Login = () => import('./../components/Login.vue')
 // const Register = () => import('./../components/Register.vue')
 // const ResetPassword = () => import('./../components/ResetPassword.vue')
 const Chat = () => import('./../components/Chat.vue')
@@ -17,7 +17,7 @@ const routes = [
     { path: '/', component: HomePage },
     { path: '/about', component: About },
     { path: '/raven', component: LoginRaven },
-    { path: '/login', component: Login },
+    // { path: '/login', component: Login },
     // { path: '/register', component: Register },
     // { path: '/resetPassword', component: ResetPassword },
     { path: '/chat', component: Chat, meta: { requiresAuth: true } },
