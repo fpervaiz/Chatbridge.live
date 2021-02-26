23aadd8967bfc4cfa0364efd1e886998d26cc4c0
Faizaan Pervaiz <fp361@cam.ac.uk>
2021-02-26 22:33:39 +0000

    feat(frontend): TCs and PP

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -88,6 +88,319 @@
         >www.flaticon.com</a
       >.
     </p>
+    <hr class="my-5" />
+    <h2>Terms and Conditions of Use</h2>
+    <p></p>
+    <h3>1. Terms</h3>
+
+    <p>
+      By accessing this Website, accessible from https://chatbridge.live, you
+      are agreeing to be bound by these Website Terms and Conditions of Use. If
+      you disagree with any of these terms, you are prohibited from accessing
+      this site.
+    </p>
+
+    <h3>2. Acceptable Use</h3>
+
+    <p>
+      You may not access or use the Website for any purpose other than that for
+      which the Website is made available. The Site may not be used in
+      connection with any commercial endeavours except those that are
+      specifically endorsed or approved by us.
+    </p>
+
+    <p>As a user of the Website, you agree not to:</p>
+
+    <ol>
+      <li>
+        Act in a manner that violates any of the
+        <router-link to="/rules">Community Rules</router-link>.
+      </li>
+      <li>
+        Systematically retrieve data or other content from the Site to create or
+        compile, directly or indirectly, a collection, compilation, database, or
+        directory without written permission from us.
+      </li>
+      <li>
+        Trick, defraud, or mislead us and other users, especially in any attempt
+        to learn sensitive account information such as user passwords.
+      </li>
+      <li>
+        Circumvent, disable, or otherwise interfere with security-related
+        features of the Site, including features that prevent or restrict the
+        use or copying of any Content or enforce limitations on the use of the
+        Site and/or the Content contained therein.
+      </li>
+      <li>
+        Use any information obtained from the Site in order to harass, abuse, or
+        harm another person.
+      </li>
+      <li>
+        Make improper use of our support services or submit false reports of
+        abuse or misconduct.
+      </li>
+      <li>
+        Use the Site in a manner inconsistent with any applicable laws or
+        regulations.
+      </li>
+      <li>Use the Site to advertise or offer to sell goods and services.</li>
+      <li>
+        Upload or transmit (or attempt to upload or to transmit) viruses, Trojan
+        horses, or other material, including excessive use of capital letters
+        and spamming (continuous posting of repetitive text), that interferes
+        with any party’s uninterrupted use and enjoyment of the Site or
+        modifies, impairs, disrupts, alters, or interferes with the use,
+        features, functions, operation, or maintenance of the Site.
+      </li>
+      <li>
+        Engage in any automated use of the system, such as using scripts to send
+        comments or messages, or using any data mining, robots, or similar data
+        gathering and extraction tools.
+      </li>
+      <li>
+        Delete the copyright or other proprietary rights notice from any
+        Content.
+      </li>
+      <li>
+        Attempt to impersonate another user or person or use the username of
+        another user.
+      </li>
+      <li>Sell or otherwise transfer your profile.</li>
+      <li>
+        Upload or transmit (or attempt to upload or to transmit) any material
+        that acts as a passive or active information collection or transmission
+        mechanism, including without limitation, clear graphics interchange
+        formats (“gifs”), 1×1 pixels, web bugs, cookies, or other similar
+        devices (sometimes referred to as “spyware” or “passive collection
+        mechanisms” or “pcms”).
+      </li>
+      <li>
+        Interfere with, disrupt, or create an undue burden on the Site or the
+        networks or services connected to the Site.
+      </li>
+      <li>
+        Harass, annoy, intimidate, or threaten any of our employees or agents
+        engaged in providing any portion of the Site to you.
+      </li>
+      <li>
+        Attempt to bypass any measures of the Site designed to prevent or
+        restrict access to the Site, or any portion of the Site.
+      </li>
+      <li>
+        Copy or adapt the Site’s software, including but not limited to Flash,
+        PHP, HTML, JavaScript, or other code.
+      </li>
+      <li>
+        Decipher, decompile, disassemble, or reverse engineer any of the
+        software comprising or in any way making up a part of the Site.
+      </li>
+      <li>
+        Except as may be the result of standard search engine or Internet
+        browser usage, use, launch, develop, or distribute any automated system,
+        including without limitation, any spider, robot, cheat utility, scraper,
+        or offline reader that accesses the Site, or using or launching any
+        unauthorized script or other software.
+      </li>
+      <li>
+        Make any unauthorized use of the Site, including collecting usernames
+        and/or email addresses of users by electronic or other means for the
+        purpose of sending unsolicited email, or creating user accounts by
+        automated means or under false pretenses.
+      </li>
+      <li>
+        Use the Site as part of any effort to compete with us or otherwise use
+        the Site and/or the Content for any revenue-generating endeavor or
+        commercial enterprise.
+      </li>
+    </ol>
+    <p></p>
+    <p>
+      Chatbridge reserves the right to, at its sole discretion, limit or deny
+      access to the Website in response to any prohibited actions you perform.
+    </p>
+
+    <h3>3. Disclaimer</h3>
+
+    <p>
+      All the materials on Chatbridge’s Website are provided "as is". Chatbridge
+      makes no warranties, may it be expressed or implied, therefore negates all
+      other warranties. Furthermore, Chatbridge does not make any
+      representations concerning the accuracy or reliability of the use of the
+      materials on its Website or otherwise relating to such materials or any
+      sites linked to this Website.
+    </p>
+
+    <h3>4. Limitations</h3>
+
+    <p>
+      Chatbridge or its suppliers will not be hold accountable for any damages
+      that will arise with the use or inability to use the materials on
+      Chatbridge’s Website, even if Chatbridge or an authorize representative of
+      this Website has been notified, orally or written, of the possibility of
+      such damage. Some jurisdiction does not allow limitations on implied
+      warranties or limitations of liability for incidental damages, these
+      limitations may not apply to you.
+    </p>
+
+    <h3>5. Revisions and Errata</h3>
+
+    <p>
+      The materials appearing on Chatbridge’s Website may include technical,
+      typographical, or photographic errors. Chatbridge will not promise that
+      any of the materials in this Website are accurate, complete, or current.
+      Chatbridge may change the materials contained on its Website at any time
+      without notice. Chatbridge does not make any commitment to update the
+      materials.
+    </p>
+
+    <h3>6. Links</h3>
+
+    <p>
+      Chatbridge has not reviewed all of the sites linked to its Website and is
+      not responsible for the contents of any such linked site. The presence of
+      any link does not imply endorsement by Chatbridge of the site. The use of
+      any linked website is at the user’s own risk.
+    </p>
+
+    <h3>7. Site Terms of Use Modifications</h3>
+
+    <p>
+      Chatbridge may revise these Terms of Use for its Website at any time
+      without prior notice. By using this Website, you are agreeing to be bound
+      by the current version of these Terms and Conditions of Use.
+    </p>
+
+    <h3>8. Your Privacy</h3>
+
+    <p>Please read our Privacy Policy.</p>
+
+    <hr class="my-5" />
+    <p></p>
+
+    <!---
+    -----
+    -----
+    -->
+
+    <h2>Privacy Policy</h2>
+
+    <p>
+      This Privacy Policy document contains types of information that is
+      collected and recorded by Chatbridge and how we use it.
+    </p>
+
+    <p>
+      If you have additional questions or require more information about our
+      Privacy Policy, do not hesitate to
+      <a href="mailto:admin@chatbridge.live">contact us</a>.
+    </p>
+
+    <p>
+      This Privacy Policy applies only to our online activities and is valid for
+      visitors to our website with regards to the information that they shared
+      and/or collect in Chatbridge. This policy is not applicable to any
+      information collected offline or via channels other than this website.
+    </p>
+
+    <h3>Consent</h3>
+
+    <p>
+      By using our website, you hereby consent to our Privacy Policy and agree
+      to its terms.
+    </p>
+
+    <h3>Information we collect</h3>
+
+    <p>
+      When you log in to Chatbridge via Raven, your initials, your surname and
+      your @cam.ac.uk email address are collected from Raven.
+    </p>
+    <p>
+      When you block or report a user, a record is made of your unique
+      identifier, the unique identifier of the user blocked/reported, the reason
+      (if applicable/given) and the time at which the block/report was
+      submitted. Randomly generated, non-attributable identifiers are used for
+      all other features and operation of the website.
+    </p>
+    <p>
+      If you contact us directly, we may receive additional information about
+      you such as your name, email address, phone number, the contents of the
+      message and/or attachments you may send us, and any other information you
+      may choose to provide.
+    </p>
+
+    <h3>How we use your information</h3>
+
+    <p>
+      We use the information we collect only as necessary to provide the
+      Chatbridge service:
+    </p>
+
+    <ul>
+      <li>Uniquely identifying you as a University of Cambridge member</li>
+      <li>Enforcing your list of blocked users</li>
+      <li>Taking action in response to user reports you have submitted</li>
+      <li>Contacting you only regarding updates to your account</li>
+    </ul>
+    <p></p>
+    <p>
+      Your information and unique identifiers are never shared with, nor
+      transmitted to, any users of the site.
+    </p>
+
+    <h3>Cookies</h3>
+
+    <p>
+      Chatbridge uses essential cookies solely to maintain your logged-in
+      session. This prevents you from having to log in every time you visit the
+      website.
+    </p>
+
+    <p>
+      For more general information on cookies, please read
+      <a target="_blank" href="https://www.cookieconsent.com/what-are-cookies/"
+        >"What Are Cookies" from Cookie Consent</a
+      >.
+    </p>
+
+    <h3>GDPR Data Protection Rights</h3>
+
+    <p>Every user is entitled to the following:</p>
+    <ul>
+      <li>
+        The right to access – You have the right to request copies of your
+        personal data.
+      </li>
+      <li>
+        The right to rectification – You have the right to request that we
+        correct any information you believe is inaccurate. You also have the
+        right to request that we complete the information you believe is
+        incomplete.
+      </li>
+      <li>
+        The right to erasure – You have the right to request that we erase your
+        personal data, under certain conditions.
+      </li>
+      <li>
+        The right to restrict processing – You have the right to request that we
+        restrict the processing of your personal data, under certain conditions.
+      </li>
+      <li>
+        The right to object to processing – You have the right to object to our
+        processing of your personal data, under certain conditions.
+      </li>
+      <li>
+        The right to data portability – You have the right to request that we
+        transfer the data that we have collected to another organization, or
+        directly to you, under certain conditions.
+      </li>
+    </ul>
+    <p></p>
+    <p>
+      If you make a request, we have one month to respond to you. If you would
+      like to exercise any of these rights, please
+      <a href="mailto:admin@chatbridge.live">contact us</a>.
+    </p>
   </v-container>
 </template>
 
