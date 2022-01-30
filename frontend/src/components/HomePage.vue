595724d45a5a4e9b7eace6783e6e7b0eaa3aa5c6
Faizaan Pervaiz <fp361@cam.ac.uk>
2022-01-30 22:27:43 +0000

    fix(frontend): remove redundant navigation

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -73,7 +73,6 @@ export default {
     logout() {
       this.$store.dispatch("logoutUserAction").then(() => {
         this.$analytics.logEvent("logout");
-        this.$router.replace("/");
       });
     },
   },
